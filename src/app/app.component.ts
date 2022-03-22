a615f6bd79395c3c9dd02f75e5fc273bfc0580be
Rodolphe <rodolphebansard@yahoo.com>
2022-03-22 22:45:49 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ecommerce-product-page';
+  // const hamburgerMenu = document.querySelector(".hamburger-menu");
+  // const overlay = document.querySelector(".overlay");
+  // const closeIcon = document.querySelector(".close-icon");
+  
+  // const articleImage = document.querySelector(".article-image");
+  
+  // const quantity = document.querySelector(".label-quantity");
+  // quantity.value = 0;
+  // quantity.innerHTML = 0;
+  // const cartArticleNumber = document.querySelector(".cart-article-number");
+  // cartArticleNumber.value = 0;
+  // cartArticleNumber.innerHTML = 0;
+  
+  // let currentImageIndex=0;
+  // let articlePrice = 150;
+  // let articleQuantity = 0;
+  // let cartItems = []
+  
+  // function toggleMenu(){
+  //     hamburgerMenu.classList.toggle("active");
+  //     overlay.classList.toggle("active");
+  // }
+  
+  // function changeImage(value){
+  //     articleImage.src = "images/image-product-" + value + ".jpg";
+  // }
+  
+  // function addItemToCart(){
+  //     if(quantity.value>0){
+  //         cartItems.push(
+  //             {
+  //                 article:articlePrice,
+  //                 qauntity:quantity.value
+  //             }
+  //         );
+  //         cartArticleNumber.value = cartArticleNumber.value + 1;
+  //         cartArticleNumber.innerHTML = cartArticleNumber.value;
+  //     }
+      
+  // }
+  
+  // function increment(){
+  //     quantity.value = quantity.value + 1;
+  //     quantity.innerHTML = quantity.value;
+  // }
+  
+  // function decrement(){
+  //     if(quantity.value > 0){
+  //         quantity.value = quantity.value - 1;
+  //         quantity.innerHTML = quantity.value;
+  //     }    
+  // }
 }
